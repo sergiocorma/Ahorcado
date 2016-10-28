b287beea1120c566762d23c66f459129d5df3b7f
alumno <alumno@aula01-15>
2016-10-28 21:34:20 +0200

    EMPEZADO CON LA PEPINO DE ahorcado

---
+++ AhorcadoGit/src/ahorcadogit/AhorcadoGit.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahorcadogit;
+
+/**
+ *
+ * @author alumno
+ */
+public class AhorcadoGit {
+
+    
+    public static void main(String[] args) {
+       
+        System.out.println("   _ _ _       ");
+        System.out.println("  |     |      ");
+        System.out.println("  |     O      ");
+        System.out.println("  |    /|/     ");
+        System.out.println("  |            ");
+        System.out.println("  |            ");
+        System.out.println("  |            ");
+        System.out.println("---------------");
+        System.out.println("---------------");
+        
+    }
+    
+}
