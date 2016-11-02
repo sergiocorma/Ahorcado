02a44ba660f0a0402890acc2f97dbcad2a5580e4
alumno <alumno@aula01-15>
2016-11-02 21:38:43 +0100

    a medias

--- AhorcadoGit/src/ahorcadogit/AhorcadoGit.java
+++ AhorcadoGit/src/ahorcadogit/AhorcadoGit.java
@@ -5,25 +5,136 @@
  */
 package ahorcadogit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
  */
 public class AhorcadoGit {
-
+    
+    public static Scanner leer = new Scanner (System.in);
+    
+    public static void partida(String vpalabra[], String verror[], String vacierto[], String palabra){
+        
+        String letra;
+        int aux= palabra.length();
+        int contador=8;
+        
+        while (contador>=0){
+            mostrarDibujo(contador);
+        
+        //mostrar vector acierto
+                    for (int j=0;j<aux;j++){
+                           System.out.print(vacierto[j]);     
+                    }
+        //pregunta letra al jugador            
+        System.out.println("Escribe una única letra: ");
+        letra=leer.next();
+        
+            if (letra.equalsIgnoreCase(vpalabra[contador])){
+                    vacierto[contador]=letra;
+                    
+            } else {
+                    contador--;
+                    
+                
+                    }
+            
+        contador++;
+        }
+        if (contador==0){
+            mostrarDibujo(contador);
+            System.out.println("¡Has perdido!");
+        
+        }
+        
+        
+    }
+    
+   
+    
+    
+   
+    
+    public static void iniciarAciertos(String vacierto[], String palabra){
+        int aux= palabra.length();
+        //ponemos donde pone null del vector
+        // guiones para darle al jugador
+        // la pista de cuantas letras son
+        for (int i=0;i<aux;i++){
+            if (vacierto[i]==null){
+                vacierto[i]=" _ ";
+            }     
+        }
+    }
+    
+    public static void separaPalabra(String vpalabra[], String palabra){
+    
+        for (int i=0; i<palabra.length();i++){
+        vpalabra[i]=palabra.substring(i,i+1);
+        }
+    
+    }
+    
+    public static void mostrarDibujo(int contador){
+      
+        switch(contador){
+            case 0:
+        System.out.println("   _ _ _       ");
+        System.out.println("  |/    |      ");
+        System.out.println("  |     0      ");
+        System.out.println("  |   /ºº/     ");
+        System.out.println("  |     |      ");
+        System.out.println("  |    / /     ");
+        System.out.println("  |            ");
+        System.out.println("---------------");
+        System.out.println("---------------");
+             break;
+            
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            case 6:
+                
+                break;
+            case 7:
+                 
+                break;
+            case 8:
+                System.out.println("---------------");
+                System.out.println("---------------");
+                break;
+                
+       }
+    }
     
     public static void main(String[] args) {
+         String palabra="kiwi";
+        // declarar vectores para la palabra escogida
+       // para la palabra con sus aciertos
+       // y para las letras erroneas
        
-        System.out.println("   _ _ _       ");
-        System.out.println("  |     |      ");
-        System.out.println("  |     O      ");
-        System.out.println("  |    /|/     ");
-        System.out.println("  |            ");
-        System.out.println("  |            ");
-        System.out.println("  |            ");
-        System.out.println("---------------");
-        System.out.println("---------------");
+        String vpalabra []= new String [palabra.length()];
+        String vacierto []= new String [palabra.length()];
+        String verror []= new String [8];
         
+       separaPalabra(vpalabra, palabra);
+       iniciarAciertos(vacierto, palabra);
+       partida(vpalabra, vacierto, verror, palabra);
+      
     }
     
 }
